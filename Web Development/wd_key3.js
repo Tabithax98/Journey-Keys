60735e714753a8703113b9b4f54b8db83d3c11a1
Tabithax98 <ofunimtabitha@gmail.com>
2025-11-30 01:33:49 +0100

    Update wd_key3.js Tabi01
    tabi web dev key 03

--- Web Development/wd_key3.js
+++ Web Development/wd_key3.js
@@ -8,34 +8,30 @@ Instructions:
 3. Run `node techlabs_checker.js 4 your_email_here` to verify and get your key.
 */
 
-const name = "your_email_here";
+const name = "tabitha";
 
 // === TASK 1 ===
 // Return a React component string that displays "Welcome <name>"
 function createComponent(name) {
-    // TODO
-    return "";
+ return `function Welcome() { return <h1>Welcome ${name}</h1>; }`;
 }
 
 // === TASK 2 ===
 // Return a simple JSX Route string like: "<Route path='/' element={<Home />} />"
 function createRoute() {
-    // TODO
-    return "";
+return "<Route path='/' element={<Home />} />";
 }
 
 // === TASK 3 ===
 // Return a React state initialization code: "const [count, setCount] = useState(0);"
 function useStateSnippet() {
-    // TODO
-    return "";
+return "const [count, setCount] = useState(0);";
 }
 
 // === TASK 4 ===
 // Return a Bootstrap button JSX: "<button class='btn btn-primary'>Click Me</button>"
 function bootstrapButton() {
-    // TODO
-    return "";
+return "<button class='btn btn-primary'>Click Me</button>";
 }
 
 module.exports = { name, createComponent, createRoute, useStateSnippet, bootstrapButton };
